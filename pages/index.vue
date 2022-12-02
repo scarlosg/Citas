cf1a070b54794c80b65c2dd7166f4a2bf2e46597
Carlos <sanchezgg1999@gmai.com>
2022-12-02 15:15:14 -0400

    Pantallas

--- pages/index.vue
+++ pages/index.vue
@@ -3,20 +3,15 @@
     <v-col cols="12" sm="8" md="6">
       <v-card class="logo py-4 d-flex justify-center">
         <NuxtLogo />
-        <VuetifyLogo />
       </v-card>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Bienvenidos a la casa de los abuelos
         </v-card-title>
         <v-card-text>
+          
           <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
+            Ejemplo
             <a
               href="https://vuetifyjs.com"
               target="_blank"
@@ -26,30 +21,12 @@
             >.
           </p>
           <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
+                       
           </p>
           <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
+            
           <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
+            TEXTO REFERENTE A LA CASA DE LOS ABUELOS
           </p>
           <div class="text-xs-right">
             <em><small>&mdash; John Leider</small></em>
@@ -73,7 +50,7 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
+ 
         </v-card-actions>
       </v-card>
     </v-col>
