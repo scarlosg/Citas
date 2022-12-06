a8d344a8604655cf4b376d62536bdea216c8ea3a
scarlosg <sanchezgg1999@gmail.com>
2022-12-06 09:11:01 -0400

    first commit

---
+++ pages/RegistrarUs.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="50"
+            label="Nombres"
+            required
+           
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="lastname"
+            :rules="nameRules"
+            :counter="30"
+            label="Apellidos"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-row align="center">
+    <v-col cols="12">
+      <v-select
+        :items="items"
+        
+        label="Cedula"
+      ></v-select>
+      <v-text-field label="Numero de Cedula"></v-text-field>
+    </v-col>
+  </v-row>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    firstname: '',
+    lastname: '',
+    Cedula: '',
+    items: ['Venezolano', 'Extranjero', 'Pasaporte',],
+    nameRules: [
+      v => !!v || 'Los Nombres son Requeridos',
+      v => v.length <= 50 || 'Muy Largo',
+    ],
+    cedula: '',
+    cedulaRules:[
+      v => !!v || 'Cedula Requerida',
+      v=> v.length <= 8 || 'Cedula no valida',
+      v=> v,
+    ],
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail es Requerido',
+      v => /.+@.+/.test(v) || 'E-mail no es valido',
+    ],
+  }),
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+        <NuxtLogo />
+      </v-card>
+      <v-card>
+        <v-card-title class="headline">
+          Bienvenidos a la casa de los abuelos
+        </v-card-title>
+        <v-card-text>
+          
+          <p>
+            Ejemplo
+            <a
+              href="https://vuetifyjs.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              documentation </a
+            >.
+          </p>
+          <p>
+                       
+          </p>
+          <p>
+            
+          <p>
+            TEXTO REFERENTE A LA CASA DE LOS ABUELOS
+          </p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          <hr class="my-3" />
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nuxt Documentation
+          </a>
+          <br />
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nuxt GitHub
+          </a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+ 
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
