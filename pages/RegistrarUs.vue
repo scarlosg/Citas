e45ba5e2262cfcf6d93a7e47611d915729ed1b06
scarlosg <sanchezgg1999@gmail.com>
2022-12-06 15:32:47 -0400

    Cambios a Registrar Us

--- pages/RegistrarUs.vue
+++ pages/RegistrarUs.vue
@@ -3,46 +3,55 @@
     <v-container>
       <v-row>
         <v-col
-          cols="12"
-          md="4"
+          cols="20"
+          md="3"
         >
-          <v-text-field
+          <v-text-field 
             v-model="firstname"
             :rules="nameRules"
             :counter="50"
             label="Nombres"
-            required
-           
+            required           
           ></v-text-field>
         </v-col>
 
         <v-col
-          cols="12"
-          md="4"
+          cols="10"
+          md="3"
         >
           <v-text-field
             v-model="lastname"
-            :rules="nameRules"
-            :counter="30"
-            label="Apellidos"
+            :rules="nameRulesA"
+            :counter="30" 
+             label="Apellidos"
             required
           ></v-text-field>
         </v-col>
 
-        <v-row align="center">
-    <v-col cols="12">
+        
+    <v-col cols="6" md="3">
       <v-select
         :items="items"
-        
         label="Cedula"
       ></v-select>
-      <v-text-field label="Numero de Cedula"></v-text-field>
+      <v-text-field v-mask="['#.###.###','##.###.###']" label="Numero de Cedula"></v-text-field>
     </v-col>
-  </v-row>
+ 
+        <v-col
+          cols="3"
+          md="3"
+        >
+          <v-text-field
+            v-model="Nacimiento"
+            v-mask="['##/##/####']"
+             label="Fecha de Nacimiento"
+            required
+          ></v-text-field>
+        </v-col>
 
         <v-col
-          cols="12"
-          md="4"
+          cols="10"
+          md="3"
         >
           <v-text-field
             v-model="email"
@@ -51,6 +60,96 @@
             required
           ></v-text-field>
         </v-col>
+
+        <v-col
+          cols="10"
+          md="3"
+        >
+          <v-text-field
+            v-model="direccion"
+            :rules="nameRulesD"
+            :counter="100" 
+             label="Dirección"
+            required
+          ></v-text-field>
+        </v-col>
+        
+        <v-col
+        class="d-flex"
+        cols="12"
+        sm="3"
+      >
+      <v-col
+          cols="3"
+          md="10"
+        >
+          <v-text-field
+            v-model="Telefono"
+            v-mask="['####-###-####']"
+            :rules="nameRulesT"
+             label="Telefono"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-col>
+      
+      <v-radio-group v-model="radioGroup">
+      <v-radio
+        v-for="n in 3"
+        :key="n"
+        :rules="Genero"
+        :label="`Femenino, Masculino, Otros  ${n}`"
+        :value="n"
+      ></v-radio>
+    </v-radio-group>
+
+      <v-col
+      class="d-flex"
+      cols="12"
+      sm="3"
+    >
+        <v-select
+          :items="itemsE"
+          label="Estado"
+          outlined
+        ></v-select>
+      </v-col>
+      
+        <v-col
+        class="d-flex"
+        cols="12"
+        sm="3"
+      >
+        <v-select
+          :items="itemsC"
+          label="Ciudad"
+        ></v-select>
+      </v-col>
+
+      <v-col
+        class="d-flex"
+        cols="12"
+        sm="3"
+      >
+        <v-select
+          :items="itemsM"
+          label="Municipio"
+          outlined
+        ></v-select>
+      </v-col>
+
+      <v-col
+        class="d-flex"
+        cols="12"
+        sm="3"
+      >
+        <v-select
+          :items="itemsP"
+          filled
+          label="Parroquia"
+        ></v-select>
+      </v-col>
+
       </v-row>
     </v-container>
   </v-form>
@@ -63,22 +162,48 @@ export default {
     firstname: '',
     lastname: '',
     Cedula: '',
+    direccion: '',
     items: ['Venezolano', 'Extranjero', 'Pasaporte',],
+    itemsE: ['Amazonas','Anzoátegui','Apure','Aragua','Barinas','Bolívar','Carabobo','Cojedes','Delta Amacuro','Distrito Federal','Falcón','Guárico','Lara','Mérida','Miranda','Monagas','Nueva Esparta','Portuguesa','Sucre','Táchira','Trujillo','Vargas','Yaracuy','Zulia',],
+    itemsC: ['Barquisimeto'],
+    itemsP: ['Ana Soto'],
+    itemsM: ['Iribarren'],
+
     nameRules: [
       v => !!v || 'Los Nombres son Requeridos',
       v => v.length <= 50 || 'Muy Largo',
     ],
-    cedula: '',
+
+    nameRulesA: [
+    v => !!v || 'Los Apellidos son Requeridos',
+      v => v.length <= 30 || 'Muy Largo',
+    ],
+
     cedulaRules:[
       v => !!v || 'Cedula Requerida',
       v=> v.length <= 8 || 'Cedula no valida',
       v=> v,
     ],
+
+    nameRulesD: [
+    v => !!v || 'Dirección Requerida',
+      v=> v.length >= 5 || 'Dirección no valida',
+      
+    ],
+
     email: '',
     emailRules: [
       v => !!v || 'E-mail es Requerido',
       v => /.+@.+/.test(v) || 'E-mail no es valido',
     ],
+    nameRulesT: [
+    v => !!v || 'Telefono Requerido',
+    ],
+
+    Genero: [
+
+    ],
+
   }),
 }
 </script>
